da4cb2fbf39e857053d39e8f4a99d0cbbbd75a10
zukhok <at89c51@sohu.com>
2023-11-21 09:17:40 +0800

    modified:   README.md new file:   main.ts new file:   package.json new file:   toolist.ts new file:   tsconfig.json

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
 # katoolin.ts
-Kali tools installer
+
+## What can this code do
+
+- Automatically install all Kali tools listed on https://www.kali.org/tools/kali-meta with prefix "kali-tools".
+
+- The new version uses a very convenient npm 2 install the program.
+
+## Support the legitimate rights & interests of programmers
+
+Supporting the 996 prohibited license while not violating the GPLv2.0 license.
+
+[![License: GPL v2](https://img.shields.io/badge/License-GPL%20v2-blue.svg)](https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html) [![996.icu](https://img.shields.io/badge/link-996.icu-red.svg)](https://996.icu) [![LICENSE](https://img.shields.io/badge/license-NPL%20(The%20996%20Prohibited%20License)-blue.svg)](https://github.com/996icu/996.ICU/blob/master/LICENSE)
+
+## Requirements
+
+- Latest node.js & globally installed typescript. (https://github.com/nvm-sh/nvm)
+- An Debian & its derivatives operating system.
+- Not supprot Redhat based distros.
+
+## Tested Distribution
+
+- Debian 12
+- Ubuntu 22.04.3 LTS
+
+## Installation
+
+```bash
+sudo apt install git
+git clone https://github.com/zukhok/katoolin.ts.git
+cd katoolin.ts
+npm i
+```
+
+## Uninstallation
+
+U can manually delete repository by removing sources from /etc/apt/sources.list, then use "apt-key del".
+
+## Usage
+
+```bash
+tsc
+node build/main.js
+```
+
+## Upgrade
+
+```bash
+git pull
+npm i
+```
+
+## Warning
+
+B4 upgrade ur system, plz make sure u have removed all Kali-linux repositories in ur `/etc/apt/sources.list`.
+
+## Have questions
+
+Plz visit https://github.com/zukhok/katoolin.ts/issues.
---
+++ main.ts
@@ -0,0 +1,146 @@
+import fs from "fs/promises";
+import readline from "node:readline/promises";
+import {$} from "zx";
+import toolist from "./toolist.js";
+let rl=readline.createInterface(process.stdin,process.stdout);
+
+async function addRepository()
+{
+  try
+  {
+    let files=await fs.readdir("/etc/apt"),deb="deb https://mirrors.tuna.tsinghua.edu.cn/kali kali-rolling main non-free contrib",deb_src="deb-src https://mirrors.tuna.tsinghua.edu.cn/kali kali-rolling main non-free contrib",content=`\n\n${deb}\n${deb_src}`;
+    if(files.includes("sources.list"))
+    {
+      content=(await fs.readFile("/etc/apt/sources.list",{encoding:"utf8"})).trim();
+      if(content.indexOf(deb)<0||content.indexOf(deb_src)<0) content+=`\n\n${deb}\n${deb_src}`;
+      content=content.trim();
+    }
+    await fs.writeFile("/etc/apt/sources.list",content);
+  
+    await fs.rm("./archive-key.asc",{force:true});
+    await $`sudo wget https://archive.kali.org/archive-key.asc`;
+    await $`sudo apt-key add archive-key.asc`;
+    await $`sudo apt update`;
+  }
+  catch(e)
+  {
+    while(true)
+    {
+      let input=await rl.question("press enter 2 continue");
+      if(input=="") return;
+    }
+  }
+}
+
+async function installPackage(packageName:string)
+{
+  console.log(`sudo apt install -y ${packageName}`);
+  await $`sudo apt install -y ${packageName}`;
+}
+
+async function uninstallPackage(packageName:string)
+{
+  console.log(`sudo apt purge --autoremove -y ${packageName}`);
+  await $`sudo apt purge --autoremove -y ${packageName}`;
+}
+
+async function install()
+{
+  while(true)
+  {
+    console.log("\x1bc");
+    console.log("install");
+    let opts=Object.keys(toolist);
+    opts.unshift("exit");
+    for(let i=0;i<opts.length;++i) console.log(`${i}) ${opts[i]}`);
+    let opt=await rl.question("option: ");
+    let failed=[];
+    if(opt=="0") return;
+    else if(opts[parseInt(opt)])
+    {
+      // @ts-ignore
+      for(let packageName of toolist[opts[parseInt(opt)]])
+      {
+        try
+        {
+          await installPackage(packageName);
+        }
+        catch(e)
+        {
+          failed.push(packageName);
+        }
+      }
+      let fixOpt=await rl.question("fix dependencies ? [Y/n] ");
+      if(fixOpt=="y"||fixOpt=="Y"||fixOpt=="")
+      {
+        if(failed.length) console.log(`failed: ${failed.join(" ")}`);
+        console.log("sudo apt install -fy");
+        await $`sudo apt install -fy`;
+      }
+    }
+    else continue;
+  }
+}
+
+async function uninstall()
+{
+  while(true)
+  {
+    console.log("\x1bc");
+    console.log("uninstall");
+    let opts=Object.keys(toolist);
+    opts.unshift("exit");
+    for(let i=0;i<opts.length;++i) console.log(`${i}) ${opts[i]}`);
+    let opt=await rl.question("option: ");
+    let failed=[];
+    if(opt=="0") return;
+    else if(opts[parseInt(opt)])
+    {
+      // @ts-ignore
+      for(let packageName of toolist[opts[parseInt(opt)]])
+      {
+        try
+        {
+          await uninstallPackage(packageName);
+        }
+        catch(e)
+        {
+          failed.push(packageName);
+        }
+      }
+      let fixOpt=await rl.question("fix dependencies ? [Y/n] ");
+      if(fixOpt=="y"||fixOpt=="Y"||fixOpt=="")
+      {
+        if(failed.length) console.log(`failed: ${failed.join(" ")}`);
+        console.log("sudo apt install -fy");
+        await $`sudo apt install -fy`;
+      }
+    }
+    else continue;
+  }
+}
+
+(async ()=>{
+  while(true)
+  {
+    console.log("\x1bc");
+    let info=
+    [
+      "home",
+      "0) exit",
+      "1) add kali repository (u can manually delete repository by removing sources from /etc/apt/sources.list, then use \"apt-key del\")",
+      "2) install",
+      "3) uninstall",
+    ].join("\n")+"\noption: ";
+    let opt=await rl.question(info);
+    if(opt=="0")
+    {
+      console.log("\x1bc");
+      process.exit(0);
+    }
+    else if(opt=="1") await addRepository();
+    else if(opt=="2") await install();
+    else if(opt=="3") await uninstall();
+    else continue;
+  }
+})();
